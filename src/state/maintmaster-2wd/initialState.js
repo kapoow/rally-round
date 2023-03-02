67e2b2cb5358113c4932b06bb7fd5fa6ec37ffbd
kapoow <19243948+kapoow@users.noreply.github.com>
2023-03-02 18:53:21 +0100

    MSRD 2WD Season 7

--- src/state/maintmaster-2wd/initialState.js
+++ src/state/maintmaster-2wd/initialState.js
@@ -2,9 +2,9 @@ const initialState = {
   nullTeamIsPrivateer: true,
   pointsForDNF: false,
   websiteName: "MaintMaster",
-  subfolderName: "supportserien",
+  subfolderName: "2wd",
   logo: "maintmaster.png",
-  siteTitlePrefix: "Supportserien",
+  siteTitlePrefix: "MaintMaster 2WD",
   useStandingsForHome: false,
   useResultsForHome: true,
   showLivePoints: true,
@@ -24,6 +24,22 @@ const initialState = {
     "background-image: linear-gradient(#00b1b1, #39d7d7); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;",
   historicalSeasonLinks: [
     {
+      name: "MaintMaster s6",
+      href: "/season6"
+    },
+    {
+      name: "Supportserien s6",
+      href: "/season6"
+    },
+    {
+      name: "Enhetscupen s6",
+      href: "/season6/enhetscupen"
+    },
+    {
+      name: "Supercupen s6",
+      href: "/season6/supercupen"
+    },
+    {
       name: "MaintMaster s5",
       href: "/season5"
     },
@@ -41,19 +57,19 @@ const initialState = {
       divisionName: "A",
       displayName: "A",
       disableSameCarValidation: true,
-      maxDriversScoringPointsForTeam: 3,
+      maxDriversScoringPointsForTeam: 2,
       filterEntries: {
         matchDivision: true,
       },
       clubs: [
         {
-          clubId: "413998",
-          championshipIds: ["676186",],
+          clubId: "387460",
+          championshipIds: ["721659",],
           includeNextChampionships: true
         },
         {
-          clubId: "387461",
-          championshipIds: ["676187",],
+          clubId: "387459",
+          championshipIds: ["721658",],
           includeNextChampionships: true
         },
       ],
@@ -62,7 +78,7 @@ const initialState = {
       ],
       points: {
         powerStage: [5,4,3,2,1],
-        overall: [30,27,25,24,23,21,19,17,15,13,11,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+        overall: [50,44,41,38,35,32,30,28,26,24,22,20,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
       }
     },
     b: {
@@ -75,31 +91,21 @@ const initialState = {
       },
       clubs: [
         {
-          clubId: "413998",
-          championshipIds: ["676186",],
+          clubId: "387460",
+          championshipIds: ["721659",],
           includeNextChampionships: true
         },
         {
-          clubId: "387461",
-          championshipIds: ["676187",],
+          clubId: "387459",
+          championshipIds: ["721658",],
           includeNextChampionships: true
         },
       ],
       events: [],
-      manualResults: [
-        {
-          eventIndex: 1,
-          results: [
-            {
-              name: "Ducce950",
-              totalTime: "33:57.777",
-            },
-          ]
-        },
-      ],
+      manualResults: [],
       points: {
         powerStage: [5,4,3,2,1],
-        overall: [30,27,25,24,23,21,19,17,15,13,11,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+        overall: [50,44,41,38,35,32,30,28,26,24,22,20,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
       }
     },   
     c: {
@@ -112,13 +118,13 @@ const initialState = {
       },
       clubs: [
         {
-          clubId: "413998",
-          championshipIds: ["676186",],
+          clubId: "387460",
+          championshipIds: ["721659",],
           includeNextChampionships: true
         },
         {
-          clubId: "387461",
-          championshipIds: ["676187",],
+          clubId: "387459",
+          championshipIds: ["721658",],
           includeNextChampionships: true
         },
       ],
@@ -127,7 +133,7 @@ const initialState = {
       ],
       points: {
         powerStage: [5,4,3,2,1],
-        overall: [30,27,25,24,23,21,19,17,15,13,11,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+        overall: [50,44,41,38,35,32,30,28,26,24,22,20,18,17,16,15,14,13,12,11,10,9,8,7,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
       },
     },
   }

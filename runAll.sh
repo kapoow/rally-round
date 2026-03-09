35a51577c59f132b641b24eef4e70309c323185c
Kapoow <jonas.gsson@gmail.com>
2026-03-09 12:06:35 +0100

    updated runAll.sh for srs-eawrc

--- runAll.sh
+++ runAll.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 echo "running all clubs"
-CLUB=jrc-themed timeout 120s node runner.js
-#CLUB=jrc-rbr-rallysprint timeout 120s node runner.js
-#CLUB=jrc-rbr timeout 120s node runner.js
-#CLUB=jrc timeout 120s node runner.js
-#CLUB=jrc-historic timeout 120s node runner.js
-#CLUB=rsc timeout 120s node runner.js
-#CLUB=oor timeout 120s node runner.js
+CLUB=srs-eawrc timeout 120s node runner.js
+#CLUB=srs-dr2 timeout 120s node runner.js
 echo "complete"
